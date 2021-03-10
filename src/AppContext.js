ea43bc5444e8b320d7900a07a3b8ba2c66b2a220
wglee0511 <ranazang@naver.com>
2021-03-10 03:55:55 +0000

    handleProductAdd => update Cart item

--- src/AppContext.js
+++ src/AppContext.js
@@ -3,16 +3,44 @@ import React,{createContext, useState, useEffect} from "react";
 export const AppContext = createContext();
 
 export function AppProvider (props) {
+    const [cart, setCart] = useState([]);
 
-    const handleProductAdd = (product) =>{
-        console.log("Add");
-        console.log(product);
+    useEffect(()=>{
+        console.log("Added to cart")
+        console.log(cart);
+    },[cart])
+
+    const handleProductAdd = (newProduct) =>{
+        const findInCart = cart.find(product => product.id === newProduct.id);
+        if (findInCart) {
+            const updateToCart = cart.map(product => {
+                if (product.id === newProduct.id){
+                    return {
+                        ...newProduct,
+                        quantity: product.quantity + 1
+                    }
+                }
+                return product
+
+            })
+            setCart(updateToCart);
+        } else {
+            const updateToCart = [
+                ...cart,
+                {
+                    ...newProduct,
+                    quantity : 1
+                }
+            ];
+            setCart(updateToCart);
+        }
+
 
     }
     const handleProductDelete = (id) =>{
         console.log("Del");
-        console.log(id);
-    }
+        
+    };
 
     const value = {
         handleProductAdd,
